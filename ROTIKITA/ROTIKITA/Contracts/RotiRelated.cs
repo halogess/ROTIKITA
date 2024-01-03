f19411f02ea0cb585c932fb744fdbc3a758f0d3a
mcpe500 <ivan04.android@gmail.com>
2024-01-03 20:01:11 +0700

    forminit

---
+++ ROTIKITA/ROTIKITA/Contracts/RotiRelated.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROTIKITA.Contracts
+{
+    public class JenisRotiStatus
+    {
+        public const int ACTIVE = 1;
+        public const int INACTIVE = 0;
+    }
+    public class JenisRotiVisible
+    {
+        public JenisRotiVisible(string jenisCode, string name, int price, int expiration)
+        {
+            this.JenisCode = jenisCode;
+            this.Name = name;
+            this.Price = price;
+            this.Expiration = expiration;
+        }
+
+        public string JenisCode { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Expiration { get; set; }
+    }
+    public class RotiVisible
+    {
+        public RotiVisible(string jenisCode, string kodeRoti, string nama, int price, int quantity, DateTime expiration)
+        {
+            this.JenisCode = jenisCode;
+            this.KodeRoti = kodeRoti;
+            this.Name = nama;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Expiration = expiration;
+        }
+
+        public string JenisCode { get; set; }
+        public string KodeRoti { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+    public class RotiCartVisible
+    {
+        public RotiCartVisible(string kode, string name, int price, int quantity, DateTime expiration)
+        {
+            this.Kode = kode;
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Expiration = expiration;
+        }
+        public string Kode { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+    public class BuyRotiItemVo
+    {
+        public string KodeRoti { get; private set; }
+        public string Nama { get; private set; }
+        public int Harga { get; private set; }
+        public int Qty { get; private set; }
+        public int Subtotal { get; private set; }
+
+        public BuyRotiItemVo(string kodeRoti, string nama, int harga, int qty, int subtotal)
+        {
+            KodeRoti = kodeRoti;
+            Nama = nama;
+            Harga = harga;
+            Qty = qty;
+            Subtotal = subtotal;
+        }
+    }
+}
---
+++ ROTIKITA/ROTIKITA/FormLogin.cs
@@ -0,0 +1,55 @@
+﻿using ROTIKITA.Controller;
+using ROTIKITA.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROTIKITA
+{
+    public partial class FormLogin : Form
+    {
+        private db_rotiEntities db = DatabaseService.GetDbContext();
+        public FormLogin()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (TBUserID.Text == "Owner" && TBPassword.Text == "Owner")
+            {
+                UserHandler.user_ID = "OWNER";
+                UserHandler.nama = "Owner";
+                UserHandler.user_ID = "OWNER00000";
+                UserHandler.role = "Owner";
+                //MainForm mainForm = new MainForm("Owner");
+                //mainForm.ShowDialog();
+            }
+            else
+            {
+                if (!UserHandler.LoginHandler(TBUserID.Text, TBPassword.Text))
+                {
+                    MessageBox.Show("Salah Login");
+                }
+                else
+                {
+                    //MainForm mainForm = new MainForm(UserHandler.role, UserHandler.nama);
+                    //mainForm.ShowDialog();
+                }
+            }
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
