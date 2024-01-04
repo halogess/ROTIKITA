e9057e3640b3c4acd635fa1515e3bd5bc82b829a
Jessica Wahyudi <jsc.wahyudi@gmail.com>
2024-01-04 20:01:47 +0700

    otw buang

--- ROTIKITA/ROTIKITA/Admin_buang.Designer.cs
+++ ROTIKITA/ROTIKITA/Admin_buang.Designer.cs
@@ -28,20 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgv_roti = new System.Windows.Forms.DataGridView();
+            this.dgv_buang = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_roti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_buang)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgv_roti
+            // 
+            this.dgv_roti.AllowUserToAddRows = false;
+            this.dgv_roti.AllowUserToDeleteRows = false;
+            this.dgv_roti.AllowUserToResizeColumns = false;
+            this.dgv_roti.AllowUserToResizeRows = false;
+            this.dgv_roti.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_roti.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv_roti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_roti.Location = new System.Drawing.Point(24, 48);
+            this.dgv_roti.Name = "dgv_roti";
+            this.dgv_roti.RowHeadersVisible = false;
+            this.dgv_roti.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv_roti.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_roti.Size = new System.Drawing.Size(656, 245);
+            this.dgv_roti.TabIndex = 22;
+            // 
+            // dgv_buang
+            // 
+            this.dgv_buang.AllowUserToAddRows = false;
+            this.dgv_buang.AllowUserToDeleteRows = false;
+            this.dgv_buang.AllowUserToResizeColumns = false;
+            this.dgv_buang.AllowUserToResizeRows = false;
+            this.dgv_buang.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_buang.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv_buang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_buang.Location = new System.Drawing.Point(24, 345);
+            this.dgv_buang.Name = "dgv_buang";
+            this.dgv_buang.RowHeadersVisible = false;
+            this.dgv_buang.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv_buang.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv_buang.Size = new System.Drawing.Size(826, 237);
+            this.dgv_buang.TabIndex = 23;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCoral;
+            this.button1.Location = new System.Drawing.Point(703, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 245);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "label1";
             // 
             // Admin_buang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(876, 618);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgv_buang);
+            this.Controls.Add(this.dgv_roti);
             this.Name = "Admin_buang";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Admin_buang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_roti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_buang)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv_roti;
+        private System.Windows.Forms.DataGridView dgv_buang;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ROTIKITA/ROTIKITA/Admin_buang.cs
+++ ROTIKITA/ROTIKITA/Admin_buang.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ROTIKITA.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,47 @@ namespace ROTIKITA
 {
     public partial class Admin_buang : Form
     {
+        db_rotiEntities db = DatabaseService.GetDbContext();
         public Admin_buang()
         {
             InitializeComponent();
         }
 
         private void Admin_buang_Load(object sender, EventArgs e)
+        {
+            load_dgv_roti();
+            dgv_buang.DataSource = (from p in db.buangs
+                                    join j in db.jenis_roti on p.kode_jenis equals j.kode_jenis
+                                    select new
+                                   {
+                                       Kode_Pembuangan = p.kode_buang,
+                                       Kode_Roti = p.kode_jenis,
+                                       Nama_Roti = j.nama,
+                                       QTY = p.qty,
+                                       Harga = p.harga,
+                                       Tanggal_Dibuang = p.dibuang_at
+                                   }).ToList();
+        }
+
+        private void load_dgv_roti()
+        {
+            dgv_roti.Columns.Clear();
+            dgv_roti.Columns.Add(new DataGridViewCheckBoxColumn());
+            dgv_roti.DataSource = (from p in db.rotis
+                                   join j in db.jenis_roti on p.kode_jenis equals j.kode_jenis
+                                   select new
+                                   {
+                                       Kode_Produksi = p.kode_roti,
+                                       Kode_Roti = p.kode_jenis,
+                                       Nama_Roti = j.nama,
+                                       QTY = p.qty,
+                                       Harga = j.harga,
+                                       Tanggal_Produksi = p.created_at,
+                                       Tanggal_Expired = p.expired_at
+                                   }).ToList();
+        }
+
+        private void dgv_roti_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
