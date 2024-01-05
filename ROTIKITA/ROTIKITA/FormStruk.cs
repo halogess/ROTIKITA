6f220a433bd30ad9e954d944f8c60e74001b5ee6
mcpe500 <ivan04.android@gmail.com>
2024-01-05 10:59:56 +0700

    fixed merge errors

---
+++ ROTIKITA/ROTIKITA/FormStruk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROTIKITA
+{
+    public partial class FormStruk : Form
+    {
+        string id = string.Empty;
+        public FormStruk(string htID)
+        {
+            InitializeComponent();
+            id = htID;
+        }
+
+        private void FormStruk_Load(object sender, EventArgs e)
+        {
+            CrystalReport2 cr2 = new CrystalReport2();
+            cr2.SetParameterValue("Htrans_id", id);
+            crystalReportViewer1.ReportSource = cr2;
+        }
+    }
+}
